94b6f04826a91db3cda6e17e7334068be780ea82
Rbang <rbangay@gmail.com>
2020-02-13 11:14:18 +1000

    Create firestore.py

---
+++ firestore.py
@@ -0,0 +1,23 @@
+from google.cloud import firestore
+
+# Project ID is determined by the GCLOUD_PROJECT environment variable
+db = firestore.Client()
+
+doc_ref = db.collection(u'users').document(u'alovelace')
+doc_ref.set({
+    u'first': u'Ada',
+    u'last': u'Lovelace',
+    u'born': 1815
+})
+doc_ref = db.collection(u'users').document(u'aturing')
+doc_ref.set({
+    u'first': u'Alan',
+    u'middle': u'Mathison',
+    u'last': u'Turing',
+    u'born': 1912
+})
+users_ref = db.collection(u'users')
+docs = users_ref.stream()
+
+for doc in docs:
+    print(u'{} => {}'.format(doc.id, doc.to_dict()))
